a4cf7e98919ee2da0732cf3e6ec28544c70b9794
bumblebeepark08-cyber <bumblebeepark08@gmail.com>
2025-12-22 11:43:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+  function openNav() {
+   document.getElementById("bar").classList.add("active");
+  }
+  function closeNav() {
+   document.getElementById("bar").classList.remove("active");
+  }
+  
+  // function openNav() {
+  //  document.getElementById("bar").style.display = "block";
+  //  document.getElementById("bar").style.right = "0";
+  // }
+
+  // function closeNav() {
+  //  document.getElementById("bar").style.right = "-400px";
+  // }
+   
+
+
+//     let popupMenu = document.getElementById('popupja');
+    
+//     if (getComputedStyle(popupMenu).display === 'none') {
+//         popupMenu.style.display = 'block';
+//     } else {
+//         popupMenu.style.display = 'none';
+//     }
+
+//     let popup = document.getElementById('divvv');
+//     popup.addEventListener('click', function () {
+//     let popupMenu = document.getElementById('popupja');
+//     popupMenu.style.display = 'none';
+// });
