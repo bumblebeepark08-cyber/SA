c72f7b624a570676a1e5006e0a21b36e5fbe8efc
bumblebeepark08-cyber <bumblebeepark08@gmail.com>
2026-05-31 10:33:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,7 @@
   function closeNav() {
    document.getElementById("bar").classList.remove("active");
   }
-  function loadPage(page){
-    document.querySelector('.foriframe').src = page;
-  }
+
   // function openNav() {
   //  document.getElementById("bar").style.display = "block";
   //  document.getElementById("bar").style.right = "0";
